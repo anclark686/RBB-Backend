4167cc9b51a2bf74f1ff3b6fc95d8e4058fb9662
anclark686 <anclark686@gmail.com>
2022-08-18 23:31:44 -0400

    changed cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'https://reyaly-black-book.netlify.app/',
+  origin: 'https://reyaly-black-book.netlify.app',
   optionsSuccessStatus: 200,
   credentials: true
 }
